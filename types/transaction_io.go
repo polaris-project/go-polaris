60f8939e0b84bc475ccf4a85b7a6c684367eeea5
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-25 13:02:38 -0500

    types: Created transaction_io.go, added transaction Bytes() helper.

---
+++ types/transaction_io.go
@@ -0,0 +1,16 @@
+// Package types provides core primitives for the operation
+// of the Polaris protocol.
+package types
+
+import "encoding/json"
+
+/* BEGIN EXPORTED METHODS */
+
+// Bytes serializes a given transaction to a byte array via json.
+func (transaction *Transaction) Bytes() []byte {
+	marshaledVal, _ := json.MarshalIndent(*transaction, "", "  ") // Marshal JSON
+
+	return marshaledVal // Return marshaled value
+}
+
+/* END EXPORTED METHODS */
