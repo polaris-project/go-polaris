2f0dcb8cbd7dd0f644b63cbd61c4a278da02987c
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-24 21:12:05 -0500

    types: Created transaction signature primitive.

---
+++ types/transaction_signature.go
@@ -0,0 +1,8 @@
+// Package types provides core primitives for the operation
+// of the Polaris protocol.
+package types
+
+// Signature is a data type representing a verifiable ECDSA signature--that of which
+// is not necessarily a transaction signature.
+type Signature struct {
+}
