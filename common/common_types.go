df25abf737f0314da76196a7b07dc97cbf37f7e3
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-25 12:55:03 -0500

    common: Created NewHash() bytes -> hash helper method.

--- common/common_types.go
+++ common/common_types.go
@@ -15,7 +15,26 @@ type Hash [HashLength]byte
 // Address represents a 20 byte, hex-encoded ECDSA public key.
 type Address [AddressLength]byte
 
+/* BEGIN EXPORTED METHODS */
+
+// NewHash converts a given hash, b, to a 32-byte mem-prefix-compliant hash.
+func NewHash(b []byte) Hash {
+	var hash Hash // Hash
+
+	bCropped := b // Init cropped buffer
+
+	if len(b) > len(hash) { // Check crop side
+		bCropped = bCropped[len(bCropped)-HashLength:] // Crop
+	}
+
+	copy(hash[HashLength-len(bCropped):], bCropped) // Copy source
+
+	return hash // Return hash value
+}
+
 // Bytes converts a given hash to a byte array.
 func (hash Hash) Bytes() []byte {
 	return hash[:] // Return byte array value
 }
+
+/* END EXPORTED METHODS */
