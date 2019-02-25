412a3e5f5d1e3270443bf4d6866220a1dc752e9a
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-25 13:01:37 -0500

    crypto: Migrated hash methods to common.Hash primitive.

--- crypto/sha3.go
+++ crypto/sha3.go
@@ -4,49 +4,52 @@ package crypto
 import (
 	"encoding/hex"
 
+	"github.com/polaris-project/go-polaris/common"
 	"golang.org/x/crypto/sha3"
 )
 
+/* BEGIN EXPORTED METHODS */
+
 // Sha3 hashes a given message via sha3.
-func Sha3(b []byte) []byte {
+func Sha3(b []byte) common.Hash {
 	hash := sha3.New256() // Init hasher
 
 	hash.Write(b) // Write
 
-	return hash.Sum(nil) // Return final hash
+	return common.NewHash(hash.Sum(nil)) // Return final hash
 }
 
 // Sha3String hashes a given message via sha3 and encodes the hashed message to a hex string.
 func Sha3String(b []byte) string {
-	b = Sha3(b) // Hash
+	b = Sha3(b).Bytes() // Hash
 
 	return hex.EncodeToString(b) // Return string
 }
 
 // Sha3n hashes a given message via sha3 n times.
-func Sha3n(b []byte, n uint) []byte {
+func Sha3n(b []byte, n uint) common.Hash {
+	hashSource := b // Init editable buffer
+
 	for x := uint(0); x != n; x++ { // Hash n times
-		b = Sha3(b) // Hash
+		hashSource = Sha3(hashSource).Bytes() // Hash
 	}
 
-	return b // Return hashed
+	return common.NewHash(hashSource) // Return hashed
 }
 
 // Sha3nString hashes a given message via sha3 n times and encodes the hashed message to a hex string.
 func Sha3nString(b []byte, n uint) string {
-	b = Sha3n(b, n) // Hash
-
-	return hex.EncodeToString(b) // Return string
+	return hex.EncodeToString(Sha3n(b, n).Bytes()) // Return string hash
 }
 
 // Sha3d hashes a given message twice via sha3.
-func Sha3d(b []byte) []byte {
-	return Sha3(Sha3(b)) // Return sha3d result
+func Sha3d(b []byte) common.Hash {
+	return Sha3(Sha3(b).Bytes()) // Return sha3d result
 }
 
 // Sha3dString hashes a given message via sha3d and encodes the hashed message to a hex string.
 func Sha3dString(b []byte) string {
-	b = Sha3d(b) // Hash
-
-	return hex.EncodeToString(b) // Return string
+	return hex.EncodeToString(Sha3d(b).Bytes()) // Return string hash
 }
+
+/* END EXPORTED METHODS */
