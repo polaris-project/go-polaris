699cb85c967bbabe829b115e402aa0a9cf1ebd7b
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-28 18:13:35 -0500

    config: Created dag_config.go.

---
+++ config/dag_config.go
@@ -0,0 +1,8 @@
+// Package config provides DAG configuration helper methods and structs.
+// Most notably, config provides the DagConfig struct, that of which is used to specify
+// supply allocations, the dag identifier, and other metadata.
+package config
+
+// DagConfig represents a DAG configuration.
+type DagConfig struct {
+}
