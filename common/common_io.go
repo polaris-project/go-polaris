7177be499248f9633f828227d88cb4242da34be0
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-28 18:47:34 -0500

    common: Created common_io_test.go.

--- common/common_io.go
+++ common/common_io.go
@@ -1,2 +1,20 @@
 // Package common defines a set of commonly used helper methods and data types.
 package common
+
+import "path/filepath"
+
+var (
+	// DataDir is the global data directory definition.
+	DataDir = getDataDir()
+)
+
+/* BEGIN INTERNAL METHODS */
+
+// getDataDir fetches the data direcotry
+func getDataDir() string {
+	abs, _ := filepath.Abs("./data") // Get absolute dir
+
+	return filepath.FromSlash(abs) // Match OS slashes
+}
+
+/* END INTERNAL METHODS */
---
+++ common/common_io_test.go
@@ -0,0 +1,12 @@
+package common
+
+import "testing"
+
+/* BEGIN INTERNAL METHODS TESTS */
+
+// TestGetDataDir tests the functionality of the getDataDir() helper method.
+func TestGetDataDir(t *testing.T) {
+	t.Log(getDataDir()) // Log data dir
+}
+
+/* END INTERNAL METHODS TESTS */
