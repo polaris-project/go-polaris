1c837f88a48346d4203f598ed8b3501df3cfcf80
Dowland Aiello <mitsukomegumii@gmail.com>
2019-02-28 20:26:57 -0500

    common: Added CreateDirIfDoesNotExist() helper.

--- common/common_io.go
+++ common/common_io.go
@@ -3,6 +3,7 @@ package common
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 )
 
@@ -13,6 +14,25 @@ var (
 	// ConfigDir is the global configuration directory definition.
 	ConfigDir = filepath.FromSlash(fmt.Sprintf("%s/config", DataDir))
 )
+
+/* BEGIN EXPORTED METHODS */
+
+// CreateDirIfDoesNotExit creates a given directory if it does not already exist.
+func CreateDirIfDoesNotExit(dir string) error {
+	safeDir := filepath.FromSlash(dir) // Just to be safe
+
+	if _, err := os.Stat(safeDir); os.IsNotExist(err) { // Check dir exists
+		err = os.MkdirAll(safeDir, 0755) // Create directory
+
+		if err != nil { // Check for errors
+			return err // Return error
+		}
+	}
+
+	return nil // No error occurred
+}
+
+/* END EXPORTED METHODS */
 
 /* BEGIN INTERNAL METHODS */
 
--- common/common_io_test.go
+++ common/common_io_test.go
@@ -2,6 +2,19 @@ package common
 
 import "testing"
 
+/* BEGIN EXPORTED METHODS TESTS */
+
+// TestCreateDirIfDoesNotExist tests the functionality of the CreateDirIfDoesNotExit() helper method.
+func TestCreateDirIfDoesNotExist(t *testing.T) {
+	err := CreateDirIfDoesNotExit(DataDir) // Create data dir (just as an example)
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
+}
+
+/* END EXPORTED METHODS TESTS */
+
 /* BEGIN INTERNAL METHODS TESTS */
 
 // TestGetDataDir tests the functionality of the getDataDir() helper method.
