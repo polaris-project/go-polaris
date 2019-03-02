63246fba77cf5f65aaae38a653ce5d240b58576e
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-01 22:39:49 -0500

    types: Added dag AddTransaction() logic.

--- types/dag.go
+++ types/dag.go
@@ -5,7 +5,6 @@ package types
 import (
 	"errors"
 	"fmt"
-	"os"
 	"path/filepath"
 	"time"
 
@@ -36,11 +35,22 @@ var (
 	// ErrNilTransactionAtHash represents an error describing a transaction pointer of nil value discovered through the
 	// querying of the working db for an invalid hash.
 	ErrNilTransactionAtHash = errors.New("no transaction exists in the dag with given hash")
+
+	// ErrNilSignature represents an error describing a transaction lacking a signature.
+	ErrNilSignature = errors.New("transaction has no signature")
+
+	// ErrDuplicateTransaction represents an error describing a duplicate transaction entry in the given working dag db.
+	ErrDuplicateTransaction = errors.New("transaction already exists in dag")
+
+	// ErrInvalidSignature represents an error describing an
+	ErrInvalidSignature = errors.New("signature invalid")
 )
 
 // Dag is a simple struct used to abstract db reading and writing methods.
 type Dag struct {
 	DagConfig *config.DagConfig `json:"config"` // Dag config
+
+	Genesis common.Hash `json:"genesis"` // Dag genesis
 }
 
 /* BEGIN EXPORTED METHODS */
@@ -60,10 +70,6 @@ func NewDag(config *config.DagConfig) (*Dag, error) {
 		return &Dag{}, err // Return found error
 	}
 
-	if _, err := os.Stat(filepath.FromSlash(fmt.Sprintf("%s/%s.db", common.DbDir, config.Identifier))); err == nil { // Check db already exists
-		return &Dag{}, ErrDagAlreadyExists // Return found error
-	}
-
 	dagDB, err := bolt.Open(filepath.FromSlash(fmt.Sprintf("%s/%s.db", common.DbDir, config.Identifier)), 0644, &bolt.Options{Timeout: 5 * time.Second}) // Open DB with timeout
 
 	if err != nil { // Check for errors
@@ -71,6 +77,12 @@ func NewDag(config *config.DagConfig) (*Dag, error) {
 	}
 
 	WorkingDagDB = dagDB // Set dag DB
+
+	err = createTransactionBucketIfNotExist() // Create transaction bucket if it doesn't already exist
+
+	if err != nil { // Check for errors
+		return &Dag{}, err // Return found error
+	}
 
 	return &Dag{
 		DagConfig: config, // Set config
@@ -81,16 +93,42 @@ func NewDag(config *config.DagConfig) (*Dag, error) {
 // Returns an ErrDagDbNotOpened error if the working dag db is nil (has been not opened).
 // Return an ErrNilTransaction error if the given transaction pointer is nil.
 // Returns an ErrDuplicateTransaction error if the transaction already exists in the working dag db.
-// Returns an ErrTransactionNotSigned error if the transaction has not been signed.
-// Return an ErrSignatureInvalid error if the transaction's signature is invalid.
+// Returns an ErrNilSignature error if the transaction has not been signed.
+// Return an ErrInvalidSignature error if the transaction's signature is invalid.
 func (dag *Dag) AddTransaction(transaction *Transaction) error {
 	if WorkingDagDB == nil { // Check dag db not opened
 		return ErrDagDbNotOpened // Return found error
 	}
 
 	if transaction == nil { // Check nil pointer
-		return ErrNilTransaction // Return error
+		return ErrNilTransaction // Return found error
 	}
+
+	if transaction.Signature == nil { // Check no signature
+		return ErrNilSignature // Return found error
+	}
+
+	err := createTransactionBucketIfNotExist() // Create transaction bucket if it doesn't already exist
+
+	if err != nil { // Check for errors
+		return err // Return found error
+	}
+
+	_, err = dag.GetTransactionByHash(transaction.Hash) // Get transaction by hash
+
+	if err == nil { // Check tx already exists
+		return ErrDuplicateTransaction // Return found error
+	}
+
+	if !transaction.Signature.Verify(transaction.Sender) { // Check transaction signature invalid
+		return ErrInvalidSignature // Return found error
+	}
+
+	err = WorkingDagDB.Update(func(tx *bolt.Tx) error {
+		workingTransactionBucket := tx.Bucket(transactionBucket) // Get transaction bucket
+
+		return workingTransactionBucket.Put(transaction.Hash.Bytes(), transaction.Bytes()) // Put transaction
+	}) // Write transaction
 
 	return nil // No error occurred, return nil
 }
@@ -104,7 +142,17 @@ func (dag *Dag) AddTransaction(transaction *Transaction) error {
 func (dag *Dag) GetTransactionByHash(transactionHash common.Hash) (*Transaction, error) {
 	var txBytes []byte // Init buffer
 
-	err := WorkingDagDB.View(func(tx *bolt.Tx) error {
+	if WorkingDagDB == nil { // Check no working db
+		return &Transaction{}, ErrDagDbNotOpened // Return found error
+	}
+
+	err := createTransactionBucketIfNotExist() // Create tx bucket if doesn't already exist to prevent nil pointer dereferences
+
+	if err != nil { // Check for errors
+		return &Transaction{}, err // Return found error
+	}
+
+	err = WorkingDagDB.View(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket(transactionBucket) // Get tx bucket
 
 		txBytes = bucket.Get(transactionHash.Bytes()) // Get tx at hash
@@ -128,3 +176,30 @@ func (dag *Dag) GetTransactionByHash(transactionHash common.Hash) (*Transaction,
 */
 
 /* END EXPORTED METHODS */
+
+/* BEGIN INTERNAL METHODS */
+
+/*
+	BEGIN DB BUCKET HELPER METHODS
+*/
+
+// createTransactionBucketIfNotExist attempts to create the "transaction" bucket in the working dag db.
+func createTransactionBucketIfNotExist() error {
+	if WorkingDagDB == nil { // Check no working db
+		return ErrDagDbNotOpened // Return found error
+	}
+
+	err := WorkingDagDB.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(transactionBucket) // Create tx bucket if it doesn't already exist
+
+		return err // Return error
+	}) // Create tx bucket if it doesn't already exist
+
+	return err // Return error
+}
+
+/*
+	END DB BUCKET HELPER METHODS
+*/
+
+/* END INTERNAL METHODS */
