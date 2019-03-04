5716b8b981a026be997a67d07a4466819ca888f6
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-04 16:45:49 -0500

    validator: Added ValidateTransactionHash().

--- validator/beacon_dag_validator.go
+++ validator/beacon_dag_validator.go
@@ -26,6 +26,11 @@ func (validator *BeaconDagValidator) ValidateTransaction(transaction *types.Tran
 	return true // Transaction is valid
 }
 
+// ValidateTransaction
+func (validator *BeaconDagValidator) ValidateTransactionHash(transaction *types.Transaction) bool {
+
+}
+
 // ValidateTransactionTimestamp validates the given transaction's timestamp against that of its parents.
 // If the timestamp of any one of the given transaction's parents is after the given transaction's timestamp, false is returned.
 // If any one of the transaction's parent transactions cannot be found in the working dag, false is returned.
