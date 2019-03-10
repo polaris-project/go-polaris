10fe4d913ac0982656d8131fe41c92f03080ba89
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-10 18:38:01 -0400

    p2p: Moved stream unit tests to dedicated file.

--- p2p/p2p_client_stream_handlers_test.go
+++ p2p/p2p_client_stream_handlers_test.go
@@ -1,11 +1,11 @@
 package p2p
 
 import (
-	"context"
 	"os"
 	"path/filepath"
 	"testing"
 
+	inet "github.com/libp2p/go-libp2p-net"
 	"github.com/polaris-project/go-polaris/config"
 	"github.com/polaris-project/go-polaris/types"
 	"github.com/polaris-project/go-polaris/validator"
@@ -13,19 +13,13 @@ import (
 
 /* BEGIN EXPORTED METHODS TESTS */
 
-// TestNewClient tests the functionality of the client initializer.
-func TestNewClient(t *testing.T) {
+// TestStartServingStream tests the functionality of the StartServingStream() helper method.
+func TestStartServingStream(t *testing.T) {
 	os.RemoveAll(filepath.FromSlash("data/db/test_network.db")) // Remove existing db
 
 	dagConfig := config.NewDagConfig(nil, "test_network", 1) // Initialize new dag config with test genesis file.
 
 	dag, err := types.NewDag(dagConfig) // Initialize dag with dag config
-
-	if err != nil { // Check for errors
-		t.Fatal(err) // Panic
-	}
-
-	_, err = NewHost(context.Background(), 2831) // Initialize host
 
 	if err != nil { // Check for errors
 		t.Fatal(err) // Panic
@@ -39,7 +33,11 @@ func TestNewClient(t *testing.T) {
 		t.Fatal("client should not be nil") // Panic
 	}
 
-	types.WorkingDagDB.Close() // Close dag db
+	err = client.StartServingStream("test_stream", func(inet.Stream) {}) // Start serving stream
+
+	if err != nil { // Check for errors
+		t.Fatal(err) // Panic
+	}
 
 	os.RemoveAll(filepath.FromSlash("data/db/test_network.db")) // Remove existing db
 }
