600c32efef5d4751ecfc00fe1397fbbbc468ec0e
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-18 21:19:09 -0400

    api: Last commit of the day.

--- api/ssl.go
+++ api/ssl.go
@@ -1,2 +1,11 @@
 // Package api contains all rpc and rest-related api helper methods, structs, etc...
 package api
+
+/* BEGIN EXPORTED METHODS */
+
+// GenerateCert generates an ssl cert.
+func GenerateCert(certName string, hosts ...string) error {
+	return nil // No error occurred, return nil
+}
+
+/* END EXPORTED METHODS */
